9535f5c5cfdddeffe8ee7e243160be15d31f40b8
lcdunne <56237933+lcdunne@users.noreply.github.com>
2022-07-31 14:36:44 +0100

    Make lots of changes and implement comparisons

--- card.py
+++ card.py
@@ -1,8 +1,11 @@
 import random
+from math import comb
 from collections import Counter
 from itertools import combinations
-from enum import Enum, auto
+from enum import Enum, auto, IntEnum
 from dataclasses import dataclass, field
+
+
 
 class ExtendedEnum(Enum):
     @classmethod
@@ -19,7 +22,8 @@ class Suit(ExtendedEnum):
     HEARTS = auto()
     SPADES = auto()
 
-class Rank(ExtendedEnum):
+class Rank(IntEnum, ExtendedEnum):
+    # Access with Rank.TWO.name, Rank.TWO.value
     # This is used because Card class is not really user-facing
     TWO = 2
     THREE = 3
@@ -50,7 +54,10 @@ class RankName(ExtendedEnum):
     KING = 'K'
     ACE = 'A'
 
-class HandStrengths(ExtendedEnum):
+class HandStrengths(IntEnum, ExtendedEnum):
+    # Ended up being used like a dictionary with many hoops just to get keys & values...
+    # Probably get rid and replace with a dict.
+    # Could also use IntEnum........................
     HIGH_CARD = 0
     PAIR = 1
     TWO_PAIR = 2
@@ -214,10 +221,9 @@ class CommunityCards:
 class Hand:
     def __init__(self, cards):
         self.cards = cards
-        self.strength = 0
         self.types = {k: [] for k in HandStrengths.items()}
         self._current_index = 0
-        self.rank_funcs = {
+        self._rank_funcs = {
             HandStrengths.ROYAL_FLUSH.name: self.is_royalflush,
             HandStrengths.STRAIGHT_FLUSH.name: self.is_straightflush,
             HandStrengths.FOUR_OF_A_KIND.name: self.is_fourofakind,
@@ -228,6 +234,7 @@ class Hand:
             HandStrengths.TWO_PAIR.name: self.is_twopair,
             HandStrengths.PAIR.name: self.is_pair,
         }
+        self.classify_hand()
     
     def __iter__(self):
         return self
@@ -241,14 +248,33 @@ class Hand:
         self._current_index += 1
         return card
     
+    def __str__(self):
+        return f"Hand: {self.strength} ({','.join([h.name for h in self.cards])})"
+    
     def __len__(self):
         return len(self.cards)
     
     def __getitem__(self, obj):
         return self.cards[obj]
-
-    def __repr__(self):
-        return ','.join([h.name for h in self.cards])
+    
+    def __eq__(self, other):
+        return self._strength.value == other._strength.value
+    
+    def __ne__(self, other):
+        return self._strength.value != other._strength.value
+    
+    def __lt__(self, other):
+        return self._strength.value < other._strength.value
+    
+    def __le__(self, other):
+        return self._strength.value <= other._strength.value
+    
+    def __gt__(self, other):
+        return self._strength.value > other._strength.value
+    
+    def __ge__(self, other):
+        return self._strength.value > other._strength.value
+    
 
     @property
     def rankings(self):
@@ -260,50 +286,80 @@ class Hand:
     
     @property
     def rankhist(self):
-        return Counter(self.rankings)
+        return dict(Counter(self.rankings).most_common())
     
     @property
     def suithist(self):
-        return Counter(self.suits)
+        return dict(Counter(self.suits).most_common())
     
-    def rank_all_hands(self):
-        self.temp = []
-        for handstrength, fun in self.rank_funcs.items():
-            hit = fun()
-            if hit:
-                print(handstrength)
-                self.temp.append(self.cards)
-                self.types[handstrength].append(self.cards)
-                break
-        if not hit:
-            # Must be a highcard.
-            self.add_handtype(HandStrengths.HIGH_CARD)
+    @property
+    def strength(self):
+        return self._strength.name
+        
+    
+    # def rank_all_hands(self):
+    #     # Rank all han
+    #     # self.temp = []
+    #     # TODO: Could sort the hands on initialisation, or could sort only when testing.
+    #     # Probably best to sort on the handspace init because then we only do it once.
+    #     for handstrength, fun in self._rank_funcs.items():
+    #         hit = fun()
+    #         if hit:
+    #             print(handstrength, Hand(self.cards))
+    #             break
+    #     #         self.temp.append(self.cards)
+    #     #         self.types[handstrength].append(self.cards)
+    #     #         break
+    #     # if not hit:
+    #     #     # Must be a highcard.
+    #     #     self.add_handtype(HandStrengths.HIGH_CARD)
+    
+    # def classify_hand(self):
+    #     for handstrength, fun in self._rank_funcs.items():
+    #         if fun():
+    #             return self, handstrength
+    #     # If it got to here without returning, handstrength is HIGH_CARD
+    #     return self, HandStrengths.HIGH_CARD.name
+    
+    def classify_hand(self):
+        for handstrength, fun in self._rank_funcs.items():
+            if fun():
+                self._strength = HandStrengths[handstrength]
+                return
+        # If it got to here without returning, handstrength is HIGH_CARD
+        self._strength = HandStrengths.HIGH_CARD.name
 
 
     # Hand Strengh Categorisation. Each one has a different check
     def is_royalflush(self):
+        # Technically indistinct from straight flush but whatever.
         return self.is_straightflush() and max(self) == 14
 
     def is_straightflush(self):
         return self.is_straight() and self.is_flush()
 
     def is_fourofakind(self):
-        return list(self.rankhist.values()) == [1, 4]
+        return list(self.rankhist.values()) == [4, 1]
 
     def is_fullhouse(self):
-        return list(self.rankhist.values()) == [2, 3]
+        return list(self.rankhist.values()) == [3, 2]
 
     def is_flush(self):
         return len(self.suithist) == 1
 
     def is_straight(self):
-        return max(self.rankings) - min(self.rankings) in [4, 12]
+        # return max(self.rankings) - min(self.rankings) in [4, 12] # If other checks pass first
+        _rankings = sorted([1 if i==14 else i for i in self.rankings])
+        return _rankings == list(range(min(_rankings), max(_rankings)+1))
+
 
     def is_threeofakind(self):
-        return list(self.rankhist.values()) == [1, 1, 3]
+        return list(self.rankhist.values()) == [3, 1, 1]
     
     def is_twopair(self):
-        return list(self.rankhist.values()) == [1, 2, 2]
+        # This one is tricky. Need the HIGHER pair of the two...
+        # Could potentially have __gt__ & __lt__ funcs for this.....
+        return list(self.rankhist.values()) == [2, 2, 1]
     
     def is_pair(self):
         return len(self.rankhist) == 4
@@ -319,7 +375,9 @@ class HandSpace:
     def __init__(self, holecards: list, community_cards: list):
         self.holecards = holecards
         self.community_cards = community_cards
-        self.space = sorted(self.holecards + self.community_cards)[::-1]
+        self.space = sorted(self.holecards + self.community_cards, reverse=True) # SORTED HERE
+        self.types = {k: [] for k in HandStrengths.items()}
+        self.madehands = {k: [] for k in HandStrengths.items()}
 
     def getcombos(self, k=None):
         # Gets all combinations in hand space
@@ -330,26 +388,85 @@ class HandSpace:
         if k > n:
             raise ValueError(f"Tried to get {k} combinations from a space of {n}")
         return list(combinations(self.space, k))
+    
+    def findbesthand(self):
+        # Loop over 7C5=21 combinations of 5-card hands
+        best_hands = []
+        for i, combo in enumerate(self.getcombos()):
+            # print(i, Hand(combo))
+            hand = Hand(combo)
+
+            # print(f"iteration {i}: {hand} {'-'*70} #")
+            # Rank all available made hands in this 5-card hand
+            # After this, hand.types must have at least high card in it.
+            # Test with.. if not hand.types: raise ValueError or something
+            hand.rank_all_hands() # now see hand.types
+            
+            # print(hand.types)
+            # for k, v in hand.types.items():
+            #     if v:
+            #         print("Adding ", k)
+            #         self.types[k].append(v)
+            # print()
+            
+            '''Problem:
+                We want to pick from the top because this will naturally be the best hand.
+                It therefore makes sense to descend through hand.types and stop when found.
+                However, HIGH_CARD types are more likely, and these are from the bottom.
+                This means we will most frequently be going close to the bottom (lots of search).
+            Solution:
+                Might be better to create a new dict 
+            '''
+            
+            # Loop over all handstrengths in desc order.
+            # If a hand is found, extract all combinations (may be > 1) and end
+            for handstrength in sorted(HandStrengths, reverse=True):
+                if hand.types.get(handstrength.name):
+                    print(f"Found {handstrength.name}\t{hand}")
+                    best_hands.append(hand.cards)
+                    break
+            # print(handstrength, [Hand(h) for h in best_hands])
+        # Fails with handspace of [9,9,9,A,A,K,K]
+        return best_hands
+
+
+
 
 
 if __name__ == '__main__':
-    # Seed 20, 39 contains flush on iterations 12,11
-    # Seed 13 contains a straight at iteration 0
-    # Seed 24 contains 3-of-a-kind
-    random.seed(24) # for repeatability
     deck = Deck()
-    deck.shuffle()
-
-    hole = deck.take(names=['7s', '2h'])
-    community = deck.take(names=['8s', '9d', 'As', 'Qh', '3d'])
-    hs = HandSpace(hole, community)
+    # random.seed(24) # for repeatability
     
-    results = []
+    # TODO: distinguish [AAA22 from 222AA]
+
+    hole = deck.take(names=['9s', 'Jh'])
+    community = deck.take(names=['Jd', '9h', '9c', '2s', '2d'])
+    hs = HandSpace(hole, community) # No sort
+    # best = hs.findbesthand()
+    # best_hand = Hand(max(best))
+    # print(best_hand)
+    # # results = []
+    
+    # Given a handspace...
+    # Loop through all combinations in the handspace
+    # Terminate on the best hand
+    # Append it to the dict of available made hands
+    MADEHANDS = {k: [] for k in HandStrengths.items()}
     for i, combo in enumerate(hs.getcombos()):
-        hand = Hand(combo)
-
-        print(f"iteration {i}: {hand} {'-'*70} #")
-
-        hand.rank_all_hands()
-        print(hand.types)
-        print()
+        hand = Hand(combo) # no sorted
+        print(f"{'-'*70} #\niteration {i}")
+        print(hand)
+        # hand.rank_all_hands()
+        # _, strength = hand.classify_hand()
+        # print(strength)
+        # MADEHANDS[strength].append(hand)
+        
+        
+        
+        # print()
+        # if strength == 'STRAIGHT':
+        #     break
+        # if hand[0].rank < max(hand).rank:
+        #     break
+    
+        
