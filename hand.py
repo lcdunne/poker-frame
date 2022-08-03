2a971a34122813d478449eab80e8216148d18edb
lcdunne <56237933+lcdunne@users.noreply.github.com>
2022-08-03 22:12:45 +0100

    Fix all hand evaluation functions and add docstrings

--- hand.py
+++ hand.py
@@ -22,11 +22,28 @@ class Hand:
     """
     def __init__(self, labels: list=None, cards=None, ):
         if labels is not None:
+            if len(set(labels)) != len(labels):
+                _found_error = [f"{c} (x{n})" for c, n in Counter(labels).most_common() if n > 1]
+                raise ValueError(f"Labels must be unique, but got: {_found_error}")
             cards = [Card(label) for label in labels]
+
         elif all([labels is None, cards is None]):
             raise ValueError("Expected either `label`, or both of `rank` and `suit`.")
+
         self.cards = cards
         self._current_index = 0
+        self._rank_funcs = {
+            # HandStrength.ROYAL_FLUSH.name: self.is_royalflush,
+            HandStrength.STRAIGHT_FLUSH.name: self.is_straightflush,
+            HandStrength.FOUR_OF_A_KIND.name: self.is_four_of_a_kind,
+            HandStrength.FULL_HOUSE.name: self.is_full_house,
+            HandStrength.FLUSH.name: self.is_flush,
+            HandStrength.STRAIGHT.name: self.is_straight,
+            HandStrength.THREE_OF_A_KIND.name: self.is_three_of_a_kind,
+            HandStrength.TWO_PAIR.name: self.is_twopair,
+            HandStrength.PAIR.name: self.is_pair,
+        }
+        # self.classify_hand()
     
     def __iter__(self):
         return self
@@ -134,44 +151,130 @@ class Hand:
         return label in self.labels
     
     def is_pair(self):
+        """Check if a 5-card hand is a pair.
+        
+        A hand like `Hand(['Ks', 'Kc', 'Ts', '8d', '2d'])` has a rank histogram 
+        of `{13: 2, 10: 1, 8: 1, 2: 1}`, which has 4 elements, and this will be 
+        the case for all hands that contain a pair.
+
+        Returns
+        -------
+        bool
+            Whether or not the hand's rank histogram has four elements.
+
+        """
         return len(self.rankhist) == 4
 
-    def is_pair(x):
-        """x: `Hand.rankhist`."""
-        return len(x) == 4
 
-    def is_twopair(x):
-        """x: `Hand.rankhist.values()`."""
-        return x == [2, 2, 1]
+    def is_twopair(self):
+        """Check if a 5-card hand is two pair.
+        
+        A hand like `Hand(['Ks', 'Kc', 'Ts', 'Td', '2d'])` has a rank histogram 
+        of `{13: 2, 10: 2, 2: 1}`, with `.values()` equal to `[2, 2, 1]`.
+
+        Returns
+        -------
+        bool
+            Whether or not the hand's rank histogram counts match for two pair
+
+        """
+        return list(self.rankhist.values()) == [2, 2, 1]
 
     def is_three_of_a_kind(x):
-        """x: `Hand.rankhist.values()`."""
+        """Check if a 5-card hand is three of a kind.
+        
+        A hand like `Hand(['Ks', 'Kc', 'Kd', 'Td', '2d'])` has a rank histogram 
+        of `{13: 3, 10: 1, 2: 1}`, with `.values()` equal to `[3, 1, 1]`.
+
+        Returns
+        -------
+        bool
+            Whether or not the hand's rank histogram counts match for three of 
+            a kind.
+
+        """
         return x == [3, 1, 1]
 
-    def is_straight(x):
-        """x: Sorted (desc) `Hand.rankings`."""
-        is_wheel = x == [14, 5, 4, 3, 2]
-        not_wheel = (len(x) == 5) and (x == list(range(max(x), min(x)-1, -1)))
-        return is_wheel or not_wheel
+    def is_straight(self):
+        """Check if a 5-card hand is a straight.
+        
+        A hand like `Hand(['4c', '3d', '5s', 'Ah', '2c'])` has a ranks list of 
+        `[14, 5, 4, 3, 2]`, and will return `True` because it is a wheel straight.
+        
+        A hand like `Hand(['Tc', '9d', '8s', '7h', '6c'])` has a ranks list of 
+        `[10, 9, 8, 7, 6]`, and will return `True` because it is a straight. In 
+        this case, the "straight pattern" is a `list` representation of 
+        `range(maxrank, minrank-1, -1)`.
 
-    def is_flush(x):
-        """x: `Hand.suithist`."""
-        return len(x) == 1
+        Returns
+        -------
+        bool
+            Whether or not the hand's rank list matches the expected pattern 
+            for a straight.
 
-    def is_fullhouse(x):
-        """x: `list(Hand.rankhist.values())`"""
-        return x == [3, 2]
+        """
+        return self.ranks in [[14, 5, 4, 3, 2], list(range(max(self.ranks), min(self.ranks)-1, -1))]
 
-    def is_four_of_a_kind(x):
-        """x: `list(Hand.rankhist.values())`"""
-        return x == [4, 1]
+    def is_flush(self):
+        """Check if a 5-card hand is a flush.
+        
+        A hand like `Hand(['Td', '3d', '8d', '7d', '6d'])` has a suits 
+        histogram of `{'DIAMONDS': 5}`, which is a single element long and is 
+        therefore a flush.
 
-    def is_straightflush(rankhist, suithist):
-        return self.is_straight(rankhist) and self.is_flush(suithist)
+        Returns
+        -------
+        bool
+            Whether or not the hand is comprised of a single suit.
+
+        """
+        
+        return len(self.suithist) == 1
+
+    def is_full_house(self):
+        """Check if a 5-card hand is a full house.
+        
+        A hand like `Hand(['Ks', 'Kc', 'Ts', 'Td', 'Th'])` has a rank histogram 
+        of `{13: 2, 10: 2, 2: 1}`, with values `[3, 2]`.
+
+        Returns
+        -------
+        bool
+            Whether or not the hand is a full house.
+        
+        """
+        return list(self.rankhist.values()) == [3, 2]
+
+    def is_four_of_a_kind(self):
+        """Check if a 5-card hand is a full house.
+        
+        A hand like `Hand(['Ks', 'Tc', 'Ts', 'Td', 'Th'])` has a rank histogram 
+        of `{10: 4, 13: 1}`, with values `[4, 1]`.
+
+        Returns
+        -------
+        bool
+            Whether or not the hand is a full house.
+        
+        """
+        return list(self.rankhist.values()) == [4, 1]
+
+    def is_straightflush(self):
+        """Check if a 5-card hand is a straight flush.
+        
+        See `Hand.is_flush` and `Hand.is_straight` for details.
+
+        Returns
+        -------
+        bool
+            Whether or not the hand is simultaneously a flush and a straight.
+
+        """
+        return self.is_straight() and self.is_flush()
 
 
 
 class HandSpace:
     pass
 
-h = Hand(['Qs', 'Qd', '9h','9d', '9c'])
+h = Hand(['Ks', 'Tc', 'Ts', 'Td', 'Th'])
